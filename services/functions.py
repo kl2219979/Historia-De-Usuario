3225da9b2346e136f13a92017f37d6e3ee4e035b
KL2219979 <KL2219979@gmail.com>
2026-03-27 11:04:36 -0500

    service functionalities

---
+++ services/functions.py
@@ -0,0 +1,17 @@
+def add_product():
+    pass
+
+def show_product():
+    pass
+
+def search_product():
+    pass
+
+def update_product():
+    pass
+
+def remove_product():
+    pass
+
+def calculate_statistics():
+    pass
