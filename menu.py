a639832a2f16ae4c96c35b6f43a4b84755a29ca9
kl2219979 <kl2219979@gmail.com>
2026-03-27 18:05:22 -0500

    advance in functionality

--- menu.py
+++ menu.py
@@ -1,4 +1,23 @@
-
+from functions import add_product, show_inventory, calculate_statistics_inventory
 
 def menu():
-    pass
+    while True:
+        print("** Menu **")
+        print("1. agregar")
+        print("2. Mostrar")
+        print("3. calcular estadisticas")
+        print("4. salir")
+        option = int(input("Ingrese el numero de la opcion que desea realizar:"))
+        
+        match option:
+            case 1:
+                add_product()
+            case 2:
+                show_inventory()
+            case 3:
+                calculate_statistics_inventory()
+            case 4:
+                print("saliendo del programa...")
+                break
+            case _:
+                print("Error, intentalo de nuevo")
--- models.py
+++ models.py
@@ -1,1 +1,5 @@
-inventory = []
+inventory = [
+    {"name":"pan","price":200,"quantity":10},
+    {"name":"leche","price":800,"quantity":20},
+    {"name":"cafe","price":700,"quantity":30}
+]
